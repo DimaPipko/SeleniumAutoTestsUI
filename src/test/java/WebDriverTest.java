b4480fe9d71e85435085d8e7ef9b83cae89dffff
DimaPipko <98277639+DimaPipko@users.noreply.github.com>
2022-06-07 00:51:20 +0300

    Selenium Webdriver HW1

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -1,11 +1,24 @@
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+
 public class WebDriverTest {
     @Test
-    public void webDriverTest(){
+    public void webDriverTest() {
+
+
         WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        System.out.println(allLinks.size());
+        WebElement tenthElement = allLinks.get(9);
+        tenthElement.click();
     }
 }
