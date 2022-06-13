8746c509c3ce849d9e2b4443870ceceeb35aa38c
DimaPipko <98277639+DimaPipko@users.noreply.github.com>
2022-06-13 16:32:53 +0300

    add sorting tests

--- src/test/java/WebDriverTest.java
+++ src/test/java/WebDriverTest.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class WebDriverTest {
@@ -45,6 +51,67 @@ public class WebDriverTest {
         WebElement TermsAndConditionElement = driver.findElement
                 (By.xpath("//ul[@class='list-horizontal']/li[contains(text(),'Terms & Conditions')]"));
         Assert.assertTrue(TermsAndConditionElement.isDisplayed());
+        driver.close();
+    }
+
+    //Webdriver advanced p2 HW
+    @Test
+    public void transitionToSubcategoryTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement rubberDucksMenuItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-1']"));
+        WebElement RDSubcategoryItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-2']"));
+        Actions action = new Actions(driver);
+        action.moveToElement(rubberDucksMenuItem).moveToElement(RDSubcategoryItem).click(RDSubcategoryItem).perform();
+        Assert.assertTrue((driver.findElement(By.xpath("//div[@id='breadcrumbs-wrapper']//li[contains(text(),'Subcategory')]"))).isDisplayed());
+        driver.close();
+    }
+
+    @Test
+    public void sortingByPriceTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement rubberDucksMenuItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-1']"));
+        WebElement RDSubcategoryItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-2']"));
+        Actions action = new Actions(driver);
+        action.moveToElement(rubberDucksMenuItem).moveToElement(RDSubcategoryItem).click(RDSubcategoryItem).perform();
+        List<WebElement> allPrices = driver.findElements(By.xpath("//div[@id='box-category']//div[@class='price-wrapper']"));
+        ArrayList<Double> doubles = new ArrayList<>();
+        for (WebElement a : allPrices) {
+            String s = a.getText();
+            Pattern pattern = Pattern.compile("\\d*[.,]?\\d+(?=[^\\d\\n]*$)");
+            Matcher matcher = pattern.matcher(s);
+            while (matcher.find()) {
+                String result = s.substring(matcher.start(), matcher.end());
+                doubles.add(Double.valueOf(result));
+            }
+        }
+        ArrayList<Double> sortedDouble = new ArrayList<>(doubles);
+        Collections.sort(sortedDouble);
+        Assert.assertEquals(doubles, sortedDouble);
+        driver.close();
+    }
+
+
+    @Test
+    public void sortingByNameTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://litecart.stqa.ru/en/");
+        WebElement rubberDucksMenuItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-1']"));
+        WebElement RDSubcategoryItem = driver.findElement(By.xpath("//div[@id='site-menu-wrapper']//li[@class='category-2']"));
+        Actions action = new Actions(driver);
+        action.moveToElement(rubberDucksMenuItem).moveToElement(RDSubcategoryItem).click(RDSubcategoryItem).perform();
+        WebElement sortByNameButton = driver.findElement(By.xpath("//a[@class='button' and contains(text(),'Name')]"));
+        sortByNameButton.click();
+        List<WebElement> allNames = driver.findElements(By.xpath("//div[@class='name']"));
+        ArrayList<String> names = new ArrayList<>();
+        for (WebElement a : allNames) {
+            String s = a.getText();
+            names.add(s);
+        }
+        ArrayList<String> sortedNames = new ArrayList<>(names);
+        Collections.sort(sortedNames);
+        Assert.assertEquals(names, sortedNames);
         driver.close();
     }
 }
