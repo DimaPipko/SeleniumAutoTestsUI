74f328afd4ebb3b9c2e29bb8481e4506b2ee91c1
DimaPipko <98277639+DimaPipko@users.noreply.github.com>
2022-07-05 04:00:21 +0300

    refactoring

--- src/test/java/PO/pageObject/pageObjectStatic/RubberDucks.java
+++ src/test/java/PO/pageObject/pageObjectStatic/RubberDucks.java
@@ -23,30 +23,4 @@ public class RubberDucks extends BasePage {
     public static boolean checkGreenDuckLabel(WebDriver driver) {
         return (driver.findElement(greenDuckLabel).getText()).equals(newLabel);
     }
-
-    //    @Test
-//    public void checkRedLabelTest() {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://litecart.stqa.ru/en/");
-//        WebElement elementRedDuck = driver.findElement(By.xpath("//a[@class='link' and contains(@title,'Red')]//div[@title]"));
-//        Assert.assertEquals(elementRedDuck.getText(), "NEW");
-//        driver.close();
-//    }
-//
-//    @Test
-//    public void checkYellowLabelTest() {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://litecart.stqa.ru/en/");
-//        WebElement elementYellowDuck = driver.findElement(By.xpath("//a[@class='link' and contains(@title,'Yellow')]//div[@title]"));
-//        Assert.assertEquals(elementYellowDuck.getText(), "SALE");
-//        driver.close();
-//    }
-//    @Test
-//    public void checkGreenLabelTest() {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://litecart.stqa.ru/en/");
-//        WebElement elementGreenDuck = driver.findElement(By.xpath("//a[@class='link' and contains(@title,'Green')]//div[@title]"));
-//        Assert.assertEquals(elementGreenDuck.getText(), "NEW");
-//        driver.close();
-//    }
 }
